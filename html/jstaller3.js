07f5cbd08da5c249b1ca9ac17fd3581592e7d9e7
Níkolas <nikolas_cardona@hotmail.com>
2020-04-18 03:40:57 -0500

    fix

--- html/jstaller3.js
+++ html/jstaller3.js
@@ -5,7 +5,7 @@ var N2 = document.getElementById("N2").value;
 var PC = (parseInt(N1)*0.3);
 var SC = (parseInt(N2)*0.3);
 var Acumulado = (PC + SC);
-var Total = 29;
+var Total = 3;
 var Necesario2 = (Total - Acumulado);
 var NotaFinal = (Necesario2 / 0.4);
 alert("Necesario: "+NotaFinal)	
