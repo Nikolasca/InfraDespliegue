2a3a1c40622e17a041c21b2d676d244039807248
Níkolas <nikolas_cardona@hotmail.com>
2020-04-18 02:37:17 -0500

    cambios

---
+++ html/jstaller3.js
@@ -0,0 +1,14 @@
+
+function funcioncalcular() {
+var N1 = document.getElementById("N1").value;
+var N2 = document.getElementById("N2").value;	
+var PC = (parseInt(N1)*0.3);
+var SC = (parseInt(N2)*0.3);
+var Acumulado = (PC + SC);
+var Total = 29;
+var Necesario2 = (Total - Acumulado);
+var NotaFinal = (Necesario2 / 0.4);
+alert("Necesario: "+NotaFinal)	
+}
+
+	
