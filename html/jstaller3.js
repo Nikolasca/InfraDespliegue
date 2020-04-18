a14c1079e0c23d364d871e54c789942c6dcb0e7c
Níkolas <nikolas_cardona@hotmail.com>
2020-04-18 03:53:00 -0500

    error este sí

--- html/jstaller3.js
+++ html/jstaller3.js
@@ -1,7 +1,7 @@
 
 function funcioncalcular() {
 var N1 = document.getElementById("N1").value;
-var N2 = document.getElementById("N2LOC").value;	
+var N2 = document.getElementById("N2").value;;;;	
 var PC = (parseInt(N1)*0.3);
 var SC = (parseInt(N2)*0.3);
 var Acumulado = (PC + SC);
