7adbe070eab458724f6476f8ce53b89128025073
Níkolas <nikolas_cardona@hotmail.com>
2020-04-18 04:12:54 -0500

    asdasd

--- html/jstaller3.js
+++ html/jstaller3.js
@@ -1,13 +1,16 @@
 
 function funcioncalcular() {
-var N1 = document.getElemenyuyuyuytById("N1").value;
-var N2 = document.getElemenddddtById("N2").value;	
+var N1 = document.getElementById("N1").value;
+var N2 = document.getElementById("N2").value;	
 var PC = (parseInt(N1)*0.3);
 var SC = (parseInt(N2)*0.3);
 var Acumulado = (PC + SC);
 var Total = 3;
 var Necesario2 = (Total - Acumulado);
 var NotaFinal = (Necesario2 / 0.4);
+array.forEach(elementasdasd => {
+    
+});
 alert("Necesario: "+NotaFinal)	
 }
 
